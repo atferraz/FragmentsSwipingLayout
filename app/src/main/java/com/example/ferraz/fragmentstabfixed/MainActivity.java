5beec4d0377a7fa0f28cc8b743b306ca375ce64a
esfd-ferraz <esfd.ferraz@gmail.com>
2015-02-26 22:14:17 +0000

    Aumento de 3 para 5 Fragments. Testes de mudança de cor nos Tabs

--- app/src/main/java/com/example/ferraz/fragmentstabfixed/MainActivity.java
+++ app/src/main/java/com/example/ferraz/fragmentstabfixed/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.ferraz.fragmentstabfixed;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 
@@ -20,9 +22,13 @@ public class MainActivity extends FragmentActivity {
         // No arranque da activity liga-se o Viewpager ao Layout da activity
         viewPager = (ViewPager) findViewById(R.id.activityMain);
 
-        // Associa o novo FragmentAdapter ao ViewPager
+        // Cria e associa um objeto FragmentAdapter ao ViewPager
         FragmentManager fm = getSupportFragmentManager();   // da support.v4
         viewPager.setAdapter(new MeuFragmentAdapter(fm));
+
+        //PagerTabStrip pagerTabStrip = (PagerTabStrip) findViewById(R.id.tabTitle);
+        //pagerTabStrip.setDrawFullUnderline(true);
+        //pagerTabStrip.setTabIndicatorColor(Color.RED);
     }
 }
 
@@ -33,44 +39,32 @@ class MeuFragmentAdapter extends FragmentStatePagerAdapter {
     }
 
     @Override
-    public Fragment getItem(int position) {         // Devolve um dos Fragments, recebida a positção
+    public int getCount() {                         // devolve o nº de Fragments tratados nesta activity
+        return 5;
+}                 // Devolve o nº de Fragments
+
+    @Override
+    public Fragment getItem(int position) {             // Devolve um dos Fragments, recebida a positção
+
         Fragment fragment = null; //
 
-        if(position == 0){
-            fragment = new FragmentS1();
-        }
-        if(position == 1){
-            fragment = new FragmentS2();
-        }
-        if(position == 2){
-            fragment = new FragmentS3();
-        }
+        if(position == 0){fragment = new FragmentS1();}
+        if(position == 1){fragment = new FragmentS2();}
+        if(position == 2){fragment = new FragmentS3();}
+        if(position == 3){fragment = new FragmentS4();}
+        if(position == 4){fragment = new FragmentS5();}
         return fragment;
     }
 
-    @Override
-    public int getCount() {                         // devolve o nº de Fragments tratados nesta activity
-        return 3;
-    }
-
-
-    // Tratamento das TABs
-
-
-
-
-    @Override
+    @Override                                           // Tratamento das TABs
     public CharSequence getPageTitle(int position){
 
-        if(position == 0){      // Fragment S1
-            return "Tab1";
-        }
-        if(position == 1){      // Fragment S2
-            return "Tab2";
-        }
-        if(position == 3){      // Fragment S3
-            return "Tab1";
-        }
+        if(position == 0){return "Tab1";}   // Fragment S1
+        if(position == 1){return "Tab2";}   // Fragment S2
+        if(position == 2){return "Tab3";}   // Fragment S3
+        if(position == 3){return "Tab4";}   // Fragment S4
+        if(position == 4){return "Tab5";}   // Fragment S5
         return null;
     }
+
 }
